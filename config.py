c098d29f76ab69a975fab18b536de08b19db47f4
SamiALqutami <samy.777659667@gmail.com>
2026-01-15 17:50:50 +0300

    Update config.py
    ن

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     ADMIN_ID = 7834574830  # تأكد أن هذا هو معرفك الصحيح
-    BOT_TOKEN = "8221859242:AAGArDTW8QjJ5zYkgdL9wm80uQCuZ9RbAFU"
+    BOT_TOKEN = "8591665308:AAFkfuzbHn2yQga4Mx6SJRGuDFZkGTkMijw"
     MONGO_URL = "mongodb+srv://sportsamy0_db_user:Samijdjdjbdj@samiq.iaq4b8z.mongodb.net/TelegramBot?retryWrites=true&w=majority&appName=samiq"
     DYNAMIC_BUTTONS = {}
